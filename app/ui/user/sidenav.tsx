436059a96fa2616d0b58ba3c5b6c6e454b6c7547
amanel <amanel.gauriat@dioptase.fr>
2025-01-14 17:35:38 +0100

    remove hover state

--- app/ui/user/sidenav.tsx
+++ app/ui/user/sidenav.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import React, { useState } from "react";
+import React from "react";
 import { UserIcon } from "@heroicons/react/24/outline";
 
 const sidenav = () => {
-  const [hover, setHover] = useState<boolean>(false);
-
   return (
-    <div
-      className="shadow-md z-10 flex h-full flex-col px-4 py-4 w-[64px] text-[0px] transition ease-in-out hover:w-64 hover:text-xl transition-all"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
+    <div className="shadow-md z-10 flex h-full flex-col px-4 py-4 w-[64px] text-[0px] transition ease-in-out hover:w-64 hover:text-xl transition-all">
       <div className="flex gap-2 items-center">
         <UserIcon className="min-w-8 max-w-8" />
         <div>Personnage</div>
