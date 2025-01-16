9db3649e69250cf7759e24b6f216c9650f1a6a29
amanel <amanel.gauriat@dioptase.fr>
2025-01-16 17:29:37 +0100

    Ajout de lien sur le logo du header

--- app/ui/user/header.tsx
+++ app/ui/user/header.tsx
@@ -1,15 +1,16 @@
 import { auth } from "@/auth";
 import LldpLogo from "../lldp-logo";
 import BurgerMenu from "./burgerMenu";
+import Link from "next/link";
 
 const header = async () => {
   const session = await auth();
 
   return (
     <div className="flex justify-between items-center h-full w-full">
-      <div className="ml-6 mr-6">
+      <Link href="/" className="ml-6 mr-6 cursor-pointer">
         <LldpLogo clickable width={78} height={48} />
-      </div>
+      </Link>
       <div className="text-4xl h-[40px]">{session?.user?.name}</div>
       <div className="ml-6 mr-6">
         <BurgerMenu />
