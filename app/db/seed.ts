3626c055dcc6ffcbc1dd43d14459aed73a17d476
Amanel <amanel.gauriat@gmail.com>
2025-08-25 19:17:34 +0200

    fix seeding 2

--- app/db/seed.ts
+++ app/db/seed.ts
@@ -30,41 +30,41 @@ async function populateSkills() {
   console.log("Skills populate");
 }
 
-async function populateRaces() {
-  console.log("Populating races");
-  const actualRaces = (
-    await db
-      .select({
-        name: raceTable.name,
-      })
-      .from(raceTable)
-  ).map((race) => race.name);
-  const toAddRaces = races.filter((race) => !actualRaces.includes(race.name));
-  toAddRaces.forEach(async (race) => {
-    const raceId = await db
-      .insert(raceTable)
-      .values({
-        name: race.name,
-        categorie: race.categorie,
-        physique: race.physique,
-        character: race.character,
-        // A ajouter
-      })
-      .onConflictDoNothing()
-      .returning({ raceId: raceTable.id });
-    await race.bonus.forEach(async (bonus) => {
-      await db.insert(raceBonusTable).values(
-        bonus!.stats.map((stats) => ({
-          race: raceId[0].raceId,
-          type: bonus!.type,
-          stat1: stats[0],
-          stat2: stats[1],
-        }))
-      );
-    });
-  });
-  console.log("Races populate");
-}
+// async function populateRaces() {
+//   console.log("Populating races");
+//   const actualRaces = (
+//     await db
+//       .select({
+//         name: raceTable.name,
+//       })
+//       .from(raceTable)
+//   ).map((race) => race.name);
+//   const toAddRaces = races.filter((race) => !actualRaces.includes(race.name));
+//   toAddRaces.forEach(async (race) => {
+//     const raceId = await db
+//       .insert(raceTable)
+//       .values({
+//         name: race.name,
+//         categorie: race.categorie,
+//         physique: race.physique,
+//         character: race.character,
+//         // A ajouter
+//       })
+//       .onConflictDoNothing()
+//       .returning({ raceId: raceTable.id });
+//     await race.bonus.forEach(async (bonus) => {
+//       await db.insert(raceBonusTable).values(
+//         bonus!.stats.map((stats) => ({
+//           race: raceId[0].raceId,
+//           type: bonus!.type,
+//           stat1: stats[0],
+//           stat2: stats[1],
+//         }))
+//       );
+//     });
+//   });
+//   console.log("Races populate");
+// }
 
 export async function GET() {
   try {
